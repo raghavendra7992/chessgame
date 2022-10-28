0c5e96ac94bb9ee4f1d748f74e1291f3ac93d268
raghavendra singh <103146867+raghavendra7992@users.noreply.github.com>
2022-10-28 21:58:06 +0530

    up

---
+++ src/components/chessbox.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import "./chessbox.css"
+const horizon=["a","b","c","d","e","f","g"]
+const vertical=[1,2,3,4,5,6,7,8]
+export default function Chessboard(){
+    return (
+        <div id="chessboard">
+        Hello
+        </div>
+    )
+}
